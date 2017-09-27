03800be6243cf7b0121449abbeb625f6aa234b26
Unknown <pratikto.sulthoni.h@mail.ugm.ac.id>
2017-09-28 01:27:08 +0700

    mbikin library i2c dev
    cuma ngedit dari library orang lain

--- Baca/baca_i2c/Debug/src/i2c.d
+++ Baca/baca_i2c/Debug/src/i2c.d
@@ -1,3 +1,48 @@
-src/i2c.o src/i2c.o: ../src/i2c.cpp ../src/i2c.h
+src/i2c.o src/i2c.o: ../src/i2c.cpp ../src/i2c.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xparameters.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xiic.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_types.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_assert.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xstatus.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xiic_l.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_io.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_printf.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xparameters.h \
+ ../../baca_i2c_bsp/microblaze_0/include/mb_interface.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_exception.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xintc.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xintc_l.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_exception.h \
+ ../../baca_i2c_bsp/microblaze_0/include/xil_printf.h
 
 ../src/i2c.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xparameters.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xiic.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_types.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_assert.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xstatus.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xiic_l.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_io.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_printf.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xparameters.h:
+
+../../baca_i2c_bsp/microblaze_0/include/mb_interface.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_exception.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xintc.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xintc_l.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_exception.h:
+
+../../baca_i2c_bsp/microblaze_0/include/xil_printf.h:
